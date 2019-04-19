8e9f4b8d89fb65f4b916f20b08e389353761fa08
TalitaCypriano <talitap.cypriano@gmail.com>
2019-04-19 13:19:23 -0300

    adding php codes

---
+++ web-code/calculator.php
@@ -0,0 +1,15 @@
+<?php
+namespace Dojo;
+
+/**
+ * Description of Calculadora
+ */
+class Calculadora
+{
+
+    public function somar($numero1, $numero2)
+    {
+        return $numero1 + $numero2;       
+    }
+
+}
